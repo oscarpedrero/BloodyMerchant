696d4c2a9af7f757051196a77cedb44285fb9828
Trodi <Trodi@DESKTOP-NA6ETDN>
2023-11-08 00:16:21 +0100

    Agregar archivos de proyecto.

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using VRising.GameData;
+using HarmonyLib;
+using Unity.Entities;
+using UnityEngine;
+using Bloodstone.API;
+using System;
+using VampireCommandFramework;
+using ProjectM;
+
+namespace BloodyMerchant
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("gg.deca.Bloodstone")]
+    [BepInDependency("gg.deca.VampireCommandFramework")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
